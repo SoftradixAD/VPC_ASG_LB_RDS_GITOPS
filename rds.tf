ca0c31d0e748e58137db04d6dd6b650e9080546f
Akshay Dhadwal <akshay@softradix.in>
2025-01-20 13:29:41 +0530

    update

--- rds.tf
+++ rds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "postgres_db" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "13.4"  # Specify the version you want
+  engine_version       = "16"  # Specify the version you want
   instance_class       = "db.t3.micro"  # Choose instance size
   db_name              = "mydb"
   username             = jsondecode(aws_secretsmanager_secret_version.db_credentials_version.secret_string).username
@@ -25,9 +25,4 @@ output "db_endpoint" {
 
 output "db_instance_id" {
   value = aws_db_instance.postgres_db.id
-}
-
-output "db_password" {
-  value = random_password.db_password.result  # Output the generated password (optional)
-  sensitive = true
 }
