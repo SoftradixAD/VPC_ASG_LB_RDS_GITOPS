a384cdaf6c6335e1616456f192e950d623f8eeb3
Akshay Dhadwal <akshay@softradix.in>
2025-01-20 11:02:55 +0530

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "local-dropbox-backup-india"
-    key            = "local-dropbox-backup-india/state/terraform.tfstate" 
+    key            = "local-dropbox-backup-india/state/gitops/terraform.tfstate" 
     region         = "ap-south-1"
     encrypt        = true                        
   }
