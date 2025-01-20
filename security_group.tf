7219022e4e32bb4ef3f354881454abb183b763d7
Akshay Dhadwal <akshay@softradix.in>
2025-01-20 12:37:32 +0530

    update

--- security_group.tf
+++ security_group.tf
@@ -27,14 +27,14 @@ resource "aws_security_group" "alb_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
